eca83ac909aac4406624197b02149ce992eabab7
donperro05 <santianqr@gmail.com>
2024-01-28 15:47:15 -0500

    Update page layout and add new dependencies

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,40 +1,58 @@
-import { Bone } from "lucide-react";
+"use client";
+
 import Link from "next/link";
 import { ModeToggle } from "~/components/mode-toggle";
 import { Button } from "~/components/ui/button";
-import { Instagram, Facebook } from "lucide-react";
+import { Instagram, Facebook, Bone } from "lucide-react";
 import { TikTok } from "./tiktok-icon";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="sticky z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="flex">
-          <div className="flex">
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-between ">
+        <div className="flex items-center">
+          <Link href="/" className="mr-6 flex items-center space-x-2">
             <Bone />
-            <span>Huellitas</span>
-          </div>
+            <span className="font-bold">Huellitas</span>
+          </Link>
 
-          <nav>
-            <Link href="/">Inicio</Link>
-            <Link href="/store">Tienda</Link>
-            <Link href="/blog">Blog</Link>
+          <nav className="flex items-center gap-6 text-sm">
+            <Link
+              href="/"
+              className={`transition-colors hover:text-foreground/80  ${pathname === "/" ? "text-foreground" : "text-foreground/60"}`}
+            >
+              Inicio
+            </Link>
+            <Link
+              href="/store"
+              className={`transition-colors hover:text-foreground/80  ${pathname === "/store" ? "text-foreground" : "text-foreground/60"}`}
+            >
+              Tienda
+            </Link>
+            <Link
+              href="/blog"
+              className={`transition-colors hover:text-foreground/80  ${pathname === "/blog" ? "text-foreground" : "text-foreground/60"}`}
+            >
+              Blog
+            </Link>
           </nav>
         </div>
-        <div>
-          <nav>
-            <Button variant="outline" size="icon">
-              <Instagram />
-            </Button>
-            <Button variant="outline" size="icon">
-              <Facebook />
-            </Button>
-            <Button variant="outline" size="icon">
-              <TikTok />
-            </Button>
-            <ModeToggle />
-          </nav>
-        </div>
+
+        <nav className="flex items-center">
+          <Button variant="ghost" size="icon">
+            <Instagram />
+          </Button>
+          <Button variant="ghost" size="icon">
+            <Facebook />
+          </Button>
+          <Button variant="ghost" size="icon">
+            <TikTok />
+          </Button>
+          <ModeToggle />
+        </nav>
       </div>
     </header>
   );
