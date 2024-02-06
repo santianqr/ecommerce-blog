ad0a212256641675bf4e14f629b6688e3f805089
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2024-02-06 12:49:05 -0500

    Refactor Home component to fetch categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,17 @@ import SocialSection from "~/components/social-section";
 import Footer from "~/components/footer";
 import { api } from "~/trpc/server";
 
-export default function Home() {
-  {
-    /*const categories_one = await api.article.getCategories.query();
-const categories_two = await api.article.getSubCategories.query();*/
-  }
+export default async function Home() {
+  const categories_one = await api.article.getCategories.query();
+  const categories_two = await api.article.getSubCategories.query();
+
   return (
     <main className="container space-y-20">
       <HeroSection />
       <FPSection />
       <SocialSection />
       <FaqSection />
-      {/*<Footer
-              categories_one={categories_one}
-              categories_two={categories_two}
-            />*/}
+      <Footer categories_one={categories_one} categories_two={categories_two} />
     </main>
   );
 }
