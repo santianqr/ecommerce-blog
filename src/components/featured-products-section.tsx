23bb3f9eef61c4c318a912e8d3fdbf5057889fda
donperro05 <santianqr@gmail.com>
2024-02-09 12:04:19 -0500

    Add featured product card to FPSection

--- src/components/featured-products-section.tsx
+++ src/components/featured-products-section.tsx
@@ -1,9 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import Image from "next/image";
+
 export default function FPSection() {
   return (
-    <section>
+    <section className="space-y-8">
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Nuestros mejores productos
       </h2>
+      <Card>
+        <CardHeader>
+          <Image
+            src="/foto_testimonio_1.jpg"
+            alt="Foto de Juan Perez"
+            width={200}
+            height={200}
+          />
+          <CardTitle className="">Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </section>
   );
 }
